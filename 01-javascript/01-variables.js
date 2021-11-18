0ed27546a6dc68f41b06e816537813ba4e86f685
Francisco García <francisco.garcia01@epn.edu.ec>
2021-11-18 08:30:21 -0500

    update/variables JS

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -69,15 +69,93 @@ else
 //objetos json
 
 const francisco = {
-    nombre:"FRANCISCO JAVIER",//lave:valor
-    false:"GARCIA MOSQUERA",
+    nombre:"FRANCISCO JAVIER",//clave:valor
+    apellido:"GARCIA MOSQUERA",
     edad:12,
     hijos:"brah",
     zapatos:"GUCCI",
     casado:undefined,
     ropa:{
         color:"magenta",
-        talla:XX,
+        talla:"XX",
     },
-    mascotas{'cachetes','tokio','lola'},
+    mascotas:['cachetes','tokio','lola'],
 };
+
+//acceder a propiedades del objeto
+
+francisco.nombre;//FRANCISCO JAVIER
+francisco.apellido;//GARCIA MOSQUERA
+francisco["nombre"];//FRANCISCO JAVIER
+console.log(francisco);
+francisco.nombre="Francisco";
+console.log(francisco);
+francisco["nombre"]="francisco";
+console.log(francisco);
+francisco.sueldo;//undefined
+console.log(francisco.sueldo);
+
+//añadir propiedades
+francisco.sueldo=1.2;
+console.log(francisco.sueldo);
+francisco["gastos"]=0.8;
+console.log(francisco.gastos);
+francisco.nombre=undefined;
+console.log(francisco);
+console.log(Object.keys(francisco));
+console.log(Object.values(francisco));
+
+//eliminar llave "nombre"
+delete francisco.nombre;
+console.log(francisco);
+
+//Variables por valor o referencia?
+//Variable por valor en JS son las primitivas (numbre, string, boolean, undefined, null)
+
+let edadFrancisco=12;
+let edadJavier=edadFrancisco;//si guardamos una primitiva en otra variable,
+                                // se guardarán por valor
+console.log(edadFrancisco);
+console.log(edadJavier);
+edadFrancisco=edadFrancisco+1;
+console.log(edadFrancisco);
+console.log(edadJavier);
+
+//variables por referencia: object ({},[])
+// let rafael={
+//     nombre:"rafael"
+// };
+// let lenin = rafael;
+// console.log(rafael);
+// console.log(lenin);
+// lenin.nombre="Lenin";
+// console.log(rafael);
+// console.log(lenin);
+// //eliminamos el valor nombre
+// delete rafael.nombre;
+// console.log(rafael);
+// console.log(lenin);
+
+let rafael={
+    nombre:"rafael"
+};
+let lenin = Object.assign({},rafael);
+console.log(rafael);
+console.log(lenin);
+lenin.nombre="Lenin";
+console.log(rafael);
+console.log(lenin);
+//eliminamos el valor nombre
+delete rafael.nombre;
+console.log(rafael);
+console.log(lenin);
+//en un vector
+let arregloNumeros=[1,2,3,4,5,6]
+let arregloClonado = Object.assign([],arregloNumeros);
+console.log(arregloNumeros);
+console.log(arregloClonado);
+arregloNumeros[0]=123;
+arregloClonado[0]=321;
+console.log(arregloNumeros);
+console.log(arregloClonado);
+

49e0065790c2d80302af0d77f6cfc83e77d718e2
Francisco García <francisco.garcia01@epn.edu.ec>
2021-11-23 09:00:47 -0500

    update/operadores

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -21,3 +21,24 @@ let objetoPrueba = {a: '1', b: '2', c: '3'};
 for (let llave in objetoPrueba) {//indices
     console.log('llave', llave);
 }
+
+console.log(arreglo);
+arreglo.push(11,12,13,14,15);//añadir al final
+console.log(arreglo);
+arreglo.pop();//eliminar un elemento al final
+console.log(arreglo);
+arreglo.unshift(5);//añadir al principio del arreglo
+console.log(arreglo);
+arreglo.splice(0,2);//eliminar desde, tantos
+console.log(arreglo);
+
+//agregar en el índice 0, 4
+arreglo.splice(0, 0, 4);
+//splice(indice, numero de elementos eliminados,...items a agregar...)
+console.log(arreglo);
+//borrar desde el 9, 2 elementos
+const indiceNueve = arreglo.indexOf(9);
+arreglo.splice(indiceNueve, 2,1112);
+console.log(arreglo);
+
+
---
+++ 01-javascript/03.operadores.js
@@ -0,0 +1,142 @@
+//03 operadores
+const arreglo = [
+    {
+        id: 1,
+        nombre: 'Adrian',
+        nota: 5
+    },
+    {
+        id: 2,
+        nombre: 'Vicente',
+        nota: 8
+    },
+    {
+        id: 3,
+        nombre: 'Carolina',
+        nota: 14
+    },
+    {
+        id: 4,
+        nombre: 'Wendy',
+        nota: 16
+    },
+    {
+        id: 5,
+        nombre: 'Andrea',
+        nota: 19
+    },
+    {
+        id: 6,
+        nombre: 'Pamela',
+        nota: 19
+    },
+    {
+        id: 7,
+        nombre: 'Cristian',
+        nota: 20
+    },
+    {
+        id: 8,
+        nombre: 'Daniel',
+        nota: 19
+    },
+    {
+        id: 9,
+        nombre: 'Lilly',
+        nota: 14
+    },
+    {
+        id: 10,
+        nombre: 'Ramiro',
+        nota: 12
+    }
+];
+
+//funciones como parámetros
+//find
+//enviamos una expresion truthy falsy y nos devuelve el primero que cumpla esta condición
+const respuestaFind = arreglo
+    .find(
+        function (valorActual, indiceActual, arregloCompleto) {
+            console.log('valorActual',valorActual);
+            console.log('indiceActual',indiceActual);
+            console.log('arregloCompleto',arregloCompleto);
+            return valorActual.nombre === "Cristian";//Expresion = = =
+        }
+    );
+console.log('respuestaFind',respuestaFind);//undefined si no existe respuesta
+
+//findindex
+//enviamos una expresion tuthy o falsy y devuelve el primero que cumpla esa condición
+const respuestaIndex = arreglo
+    .findIndex(
+        function (valorActual, indiceActual, arregloCompleto) {
+            return valorActual.nombre === "Cristian";
+        }
+    );
+console.log('respuestaIndex', respuestaIndex);//al no encontrar es -1
+//el mas seguro para usar con objetos
+
+//for each
+//itera el arreglo
+const respuestaFor = arreglo
+    .forEach(
+        function (valorActual, indiceActual, arregloCompleto) {
+            console.log('valorActual', valorActual);
+        }
+    );
+console.log('respuestaFor', respuestaFor);
+
+//map
+//modificar el arreglo y devuelve un nuevo arreglo
+//enviamos datos del nuevo arreglo
+//retorna el nuevo arreglo
+const respuestaMap = arreglo
+    .map(
+        (valorActual, indiceActual, arregloCompleto) => {
+            const nuevoElemento = {
+                //id: valorActual.id,
+                nombre: valorActual.nombre,
+                nota: valorActual.nota + 1,
+            };
+            return nuevoElemento;
+        }
+    );
+console.log('arreglo', arreglo);
+console.log('respuestaMap', respuestaMap);
+
+//filter
+//enviamos expresion truthy falsy
+//devuelve los elementos que cumplen con esa condición
+const respuestaFilter = arreglo
+    .filter(
+        (valorActual, indiceActual, arregloCompleto) => {
+            return valorActual.nota >= 14;
+        }
+    );
+console.log('arreglo', arreglo);
+console.log('respuestaFilter', respuestaFilter);
+
+//some -> expresion
+//devuelve booleano
+//Existe alguna expresion? SI o NO
+//OR
+const respuestaSome = arreglo
+    .some(
+        (valorActual, indiceActual, arregloCompleto) => {
+            return valorActual.nota < 9;
+        }
+    );
+console.log('respuestaSome', respuestaSome);
+
+//every -> expresion
+//devuelve booleano
+//Existe expresion en todos? SI o NO
+//AND
+const respuestaEvery = arreglo
+    .every(
+        (valorActual, indiceActual, arregloCompleto) => {
+            return valorActual.nota > 14;
+        }
+    );
+console.log('respuestaEvery', respuestaEvery);

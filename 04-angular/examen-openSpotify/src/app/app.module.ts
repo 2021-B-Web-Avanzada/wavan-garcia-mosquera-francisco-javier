0db32fa85fb00c8895e703b565e2c5648c906be6
Francisco García <francisco.garcia01@epn.edu.ec>
2022-01-10 23:30:25 -0500

    update/examen Search Bar

--- 04-angular/examen-openSpotify/src/app/app.module.ts
+++ 04-angular/examen-openSpotify/src/app/app.module.ts
@@ -5,11 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReproductorComponent } from './componentes/reproductor/reproductor.component';
 import { MenuLateralComponent } from './componentes/menu-lateral/menu-lateral.component';
-import { BotonSeccionComponent } from './componentes/boton-seccion/boton-seccion.component';
 import { BotonRecomendacionInicialComponent } from './componentes/boton-recomendacion-inicial/boton-recomendacion-inicial.component';
 import { TarjetaRecomendacionComponent } from './componentes/tarjeta-recomendacion/tarjeta-recomendacion.component';
 import { BotonRecomendacionComunComponent } from './componentes/boton-recomendacion-comun/boton-recomendacion-comun.component';
-import { BotonRecomendacionGeneroComponent } from './componentes/boton-recomendacion-genero/boton-recomendacion-genero.component';
 import { BotonComunGeneroComponent } from './componentes/boton-comun-genero/boton-comun-genero.component';
 import { RutaInicioComponent } from './rutas/ruta-inicio-component/ruta-inicio.component';
 import { RutaBuscarComponent } from './rutas/ruta-buscar-component/ruta-buscar.component';
@@ -20,11 +18,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppComponent,
     ReproductorComponent,
     MenuLateralComponent,
-    BotonSeccionComponent,
     BotonRecomendacionInicialComponent,
     TarjetaRecomendacionComponent,
     BotonRecomendacionComunComponent,
-    BotonRecomendacionGeneroComponent,
     BotonComunGeneroComponent,
     RutaInicioComponent,
     RutaBuscarComponent
